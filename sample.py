5ad67799fd001dc4f5d324e6c118c97986bcd9dc
Diana <dianasokolova5843@gmail.com>
2021-01-04 22:42:48 +0300

    add function

--- sample.py
+++ sample.py
@@ -1,2 +1,18 @@
-# from datetime import date, timedelta, datetime
+from datetime import timedelta, datetime
+import time
+
+def equal(dt1, dt2):
+    return dt1.replace(second=0, microsecond=0) == dt2.replase(second=0, microsecond=0)
+
+
+
+time.strftime("%w") #[0,6] voskres 0
+
+time.strftime("%u") #[1, 7] poned 1
+
+datetime.date.weekday() #[0,6] poned 0
+
+datetime.date.isoweekday() #[1, 7] poned 1
+
+calendar.weekday() #[0, 6] poned 0
 
