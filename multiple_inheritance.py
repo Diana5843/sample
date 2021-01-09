8ffeb95710347c422898b1337b2c7c4c933d21dd
Diana <dianasokolova5843@gmail.com>
2021-01-10 00:54:49 +0300

    class Mecanical

--- multiple_inheritance.py
+++ multiple_inheritance.py
@@ -1,15 +1,26 @@
+class Mecanical:
+    def swim(self):
+        print("Механизм плывет в океане")
+
+
 class Auto:
-    def ride(self):
-        print("Автомобиль едет по дороге")
+    def swim(self):
+        print("Автомобиль плывет в океане")
 
 
 class Boat:
     def swim(self):
         print("Лодка плывет в океане")
 
+
 class Amphibiah(Auto, Boat):
     pass
 
+
 a = Amphibiah()
-a.ride()
 a.swim()
+
+# print(isinstance(a, Amphibiah))
+# print(isinstance(a, Auto))
+# print(isinstance(a, Boat))
+
